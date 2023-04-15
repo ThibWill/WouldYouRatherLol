8ca640665606634f468cb732019603c7821892c1
ThibWill <thibault.willer@gmail.com>
2023-04-15 15:51:40 +0200

    🌱 Add initial datas with seed

---
+++ prisma/initialDatas.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const champions = [
+  'Akali',
+  'Brand',
+  'Kennen',
+  'Shen',
+  'Alistar',
+  'Zoe',
+  'Yuumi',
+  'Graves',
+  'Anivia'
+]
+
+async function main() {
+  const promisesChampion = champions.map(championName => prisma.champion.create({
+    data: {
+      name: championName,
+      votes: 0
+    },
+  }))
+  Promise.all(promisesChampion)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
