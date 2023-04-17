4cfc9b75a324b3f52f58f2f5bfa25653a2902b50
ThibWill <thibault.willer@gmail.com>
2023-04-17 19:56:23 +0200

    💫 API spasharts

--- pages/versus.tsx
+++ pages/versus.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image';
+import { URLSplashArt } from '../services/communityDragon';
+
+export function championsVersusSplashart(championName1: string, championName2: string): Array<JSX.Element> {
+  return [
+    <Image width="500" height="500" src={URLSplashArt(championName1)} alt={championName1}></Image>,
+    <Image width="500" height="500" src={URLSplashArt(championName2)} alt={championName2}></Image>
+  ]
+}
+
 export default function Versus() {
   return (
     <>
       <main className="flex flex-row min-h-full justify-between items-stretch">
         <section className='bg-green grow'>
         </section>
-          Image 1
+          {championsVersusSplashart('alistar', 'anivia')}
         <section className='bg-blue grow'>
           Image 2
         </section>
---
+++ services/communityDragon.ts
@@ -0,0 +1,5 @@
+const BASE_URL_CD = 'https://cdn.communitydragon.org/'
+
+export function URLSplashArt(championName: string) {
+  return `${BASE_URL_CD}/latest/champion/${championName}/splash-art`;
+}
